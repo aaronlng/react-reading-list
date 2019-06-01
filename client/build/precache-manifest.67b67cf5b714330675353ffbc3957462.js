d87c6716a1d2196c6a59d4a68774b189dd6d2392
Aaron Long <aaronlng05@gmail.com>
2019-06-01 13:51:47 -0400

    initial commit

---
+++ client/build/precache-manifest.67b67cf5b714330675353ffbc3957462.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "35a4d5466698a3deab41",
+    "url": "/static/js/main.fe00b62c.chunk.js"
+  },
+  {
+    "revision": "73a92ea81af2477f9057",
+    "url": "/static/js/2.44b3b530.chunk.js"
+  },
+  {
+    "revision": "35a4d5466698a3deab41",
+    "url": "/static/css/main.8c476e21.chunk.css"
+  },
+  {
+    "revision": "3810058425f32646a77a22c61f9b702b",
+    "url": "/index.html"
+  }
+];
